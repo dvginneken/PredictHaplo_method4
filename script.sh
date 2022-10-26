c1ce6b639a4e6f4406511cddb3a13af3ba1b2cd5
Daphne <34064919+dvginneken@users.noreply.github.com>
2022-10-26 13:52:44 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,23 +1,18 @@
-#!/bin/bash
-#SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --mail-user=M.I.vanGinneken@umcutrecht.nl
 
+output_dir="$1"
+haplohiv_folder="$2"
 
-
-output_dir="patient2"
-haplohiv_folder="/hpc/dla_lti/dvanginneken/HaploHIV_Daphne/haplohiv2/patient2_out/"
-table="/hpc/dla_lti/dvanginneken/MergedTimepoints_PredictHaplo/Mastertable_timepoints.tsv"
-
+# Retrieve the name and locations of the sample consensus files to be taken together (combinations)
 mkdir -p ${output_dir}/consensus
 if [ "$(ls -A ${output_dir}/consensus)" ]; then
     echo "    Already grepped consensus name"
 else
     for sample in `ls ${haplohiv_folder}/ec`
     do
-        Rscript ConsensusFiles.R $table $haplohiv_folder $output_dir $sample
+        Rscript ConsensusFiles.R "mastertable.tsv" $haplohiv_folder $output_dir $sample
     done
 fi
 
-
-snakemake -j 8 --config output_dir=$output_dir haplohiv_folder=$haplohiv_folder table=$table --
+# Run the haplotyping pipeline
+snakemake -j 8 --config output_dir=$output_dir haplohiv_folder=$haplohiv_folder --
 
